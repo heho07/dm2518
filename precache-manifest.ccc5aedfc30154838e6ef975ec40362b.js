73aa67d956220660ab40967664617a5464c3e04f
Herman <hermanor@kth.se>
2019-03-26 16:01:18 +0100

    Updates

---
+++ precache-manifest.ccc5aedfc30154838e6ef975ec40362b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dm2518/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9dedf7fe4429651039ab",
+    "url": "/dm2518/static/js/runtime~main.17d095a9.js"
+  },
+  {
+    "revision": "d1ea1edc393a89ba1356",
+    "url": "/dm2518/static/js/main.8102347c.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/dm2518/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "d1ea1edc393a89ba1356",
+    "url": "/dm2518/static/css/main.3c86ece5.chunk.css"
+  },
+  {
+    "revision": "05900878bcd61baa5e2f525013b1f059",
+    "url": "/dm2518/index.html"
+  }
+];
