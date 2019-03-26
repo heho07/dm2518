0bee7d29f88b6297ead7f9ac401d5abdabfc8de5
Herman <hermanor@kth.se>
2019-03-26 16:18:37 +0100

    testar ifall changes händer online på branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-          HELLO WORLD!123
+          HELLO WORLD!123. Does this appear online?
           </p>
           <Karta/>
         </header>
