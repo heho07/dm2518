fe6e534e01c2cdd7a59c18776e2217f3771b7a8f
Mikael Forsmark <mforsmar@kth.se>
2019-03-26 16:29:27 +0100

    La tillbaka 1234

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-          HELLO WORLD!
+          HELLO WORLD1234!
           </p>
           <Karta/>
         </header>
