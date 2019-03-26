293925037ef2fd4a13acd5ac949d2e12e354f05d
Herman <hermanor@kth.se>
2019-03-26 16:09:11 +0100

    hello world

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-            HELLO WORLD!
+          HELLO WORLD!
           </p>
           <a
             className="App-link"
