7e2090783564e289611da18961b95ca16688c650
Herman <hermanor@kth.se>
2019-03-27 13:20:37 +0100

    zoom knappar fungerar

--- src/Karta/Karta.js
+++ src/Karta/Karta.js
@@ -3,6 +3,13 @@ import React, { Component } from 'react';
 
 class Karta extends Component {
     
+  constructor(props){
+    super(props);
+    this.state = {
+      map:null,
+    }
+  }
+  
   componentDidMount(){
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 59.3498092, lng: 18.0684758},
@@ -12,17 +19,25 @@ class Karta extends Component {
       gestureHandling: 'none',
       zoomControl: false
     });
+    this.setState({map:map});
 
   }
 
+  zoom(number){
+    console.log("empty");
+    let currentZoom = this.state.map.getZoom();
+    this.state.map.setZoom(currentZoom + number);
+  }
 
   render() {
 
     return (
-      <div id="map" style={{width:500, height:500}}>
-          
-          
-        
+      <div>
+        <div id="map" style={{width:500, height:500}}>
+        </div>
+        <button onClick = {() => this.zoom(1)}>Zoom in one level</button>
+        <button onClick = {() => this.zoom(-1)}>Zoom out one level</button>
+        <p>{this.state.test}</p>
       </div>
     );
   }
