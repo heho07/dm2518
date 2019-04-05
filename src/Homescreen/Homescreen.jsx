913e50924fb6ae60451b2f395bb006ea4b1959e6
Herman <hermanor@kth.se>
2019-04-05 14:45:00 +0200

    basic routing sort of complete

--- src/Homescreen/Homescreen.jsx
+++ src/Homescreen/Homescreen.jsx
@@ -12,15 +12,25 @@ import 'onsenui/css/onsen-css-components.css';
 
 class Homescreen extends Component {
     
-
+	handleClick(){
+		//ons.notification.alert('clicked');
+	}
 
   render() {
-
+//<h1>Welcome to The Map App!</h1>
+  //      <h3>Click on the map below to start the experience&trade;</h3>
+    //    <Link to = "/Yoga"><img src={require("../images/favicon.png")}/></Link>
+    let data = ["Yoga", "Meditation", "Breathing", "About"];
     return (
       <Ons.Page>
-		<h1>Welcome to The Map App!</h1>
-        <h3>Click on the map below to start the experience&trade;</h3>
-        <Link to = "/Yoga"><img src={require("../images/favicon.png")}/></Link>
+		<Ons.List
+     		dataSource= {data}
+     		renderHeader={() =>
+        		<Ons.ListHeader style={{fontSize: 15}} className="testClass"> Header Text </Ons.ListHeader> }
+    		renderRow = {(row, idx) => (
+      			<Ons.ListItem onClick = {() => this.handleClick()} key = {row}> <Link to = {"/" + row}> {row} </Link> </Ons.ListItem>
+			)}
+  		/>	
         
       </Ons.Page>
     );
