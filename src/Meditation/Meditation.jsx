bbf87a1b63f74783ad363a0df1dc1b8f83fe48a6
Herman <hermanor@kth.se>
2019-04-08 14:18:59 +0200

    added images

--- src/Meditation/Meditation.jsx
+++ src/Meditation/Meditation.jsx
@@ -26,6 +26,28 @@ class Meditation extends Component {
     ons.notification.alert('Hello world!');
   }
 
+  instructions(){
+    let instructions = ["Stand on your feet.", "Experience the contact with the ground.", "Feel into the soles of the feet.", 
+    "Feel the knees.", "The hips.", "The spine.", "Shoulders.", "Neck.", "Head.", "Feel the connection between the top of the head and the feet."];        
+    
+    return (
+    <div> 
+      <h1>Feel Your Feet</h1>
+      <Ons.List 
+        dataSource = {instructions}
+        renderRow = {(row, idx) => (
+          <Ons.ListItem key = {row}>
+            {row}
+          </Ons.ListItem>
+          )
+        }
+        renderFooter = {this.renderFooter}
+        >
+      </Ons.List>
+    </div>
+    )
+  }
+
   render() {
        
     return (
@@ -36,7 +58,10 @@ class Meditation extends Component {
               <div className="center">{this.props.title}</div>
             </Ons.Toolbar>
           }>
-            <Ons.Button onClick={this.handleClick}>Tap me!</Ons.Button>
+            <Ons.Card>
+              <img src = "../images/photo-feet.jpg"/>
+              {this.instructions()}
+            </Ons.Card>
           </Ons.Page>
       );
   }
