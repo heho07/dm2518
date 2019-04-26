25c1ec4b095f34d2deb2262d7a5615a1e09049d7
Herman <hermanor@kth.se>
2019-04-26 11:23:31 +0200

    installerat pubnub-react. tagit bort onödigt, klistrat in kod för helloworld i pubnub. lagt till keys.js i gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 
-import Yoga from "./Yoga/Yoga";
-import Meditation from "./Meditation/Meditation";
-import Breathing from "./Breathing/Breathing";
-import About from "./About/About";
 import Homescreen from "./Homescreen/Homescreen";
 // imports for OnsenUI
 import * as Ons from 'react-onsenui'; // Import everything and use it as 'Ons.Page', 'Ons.Button'
--- src/Homescreen/Homescreen.jsx
+++ src/Homescreen/Homescreen.jsx
@@ -1,72 +1,50 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
-import "./Homescreen.css";
-
-
-import Yoga from "../Yoga/Yoga";
-import Meditation from "../Meditation/Meditation";
-import Breathing from "../Breathing/Breathing";
-import About from "../About/About";
-
-
-// imports for OnsenUI
-import * as Ons from 'react-onsenui'; // Import everything and use it as 'Ons.Page', 'Ons.Button'
-import * as ons from 'onsenui'; // This needs to be imported to bootstrap the components.
-// Webpack CSS import
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
-
-
-class Homescreen extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      index:0,
+import PubNubReact from 'pubnub-react';
+import keys from "./keys";
+ 
+export default class Homescreen extends Component {
+    constructor(props) {
+        super(props);
+        this.pubnub = new PubNubReact({
+            publishKey: keys.publish,
+            subscribeKey: keys.subscribe
+        });
+        this.pubnub.init(this);
     }
-  }
-    
-
-  render() {
-    let data = ["Yoga", "Meditation", "Breathing", "About"];
-
-
-    // Ons.Tabbar skapar tabbaren där nere
-    // det man matar in är vad som ska visas på varje tab genom "content".
-    // där lägger man helt enkelt in sitt element
-    // Ons.Tab är den ruta som man kan klicka på i tabbar där nere och "label" är vad som ska stå där.
-    // TODO: snygga till och byta icon
-    return (
-        <Ons.Page>  
-          <Ons.Tabbar
-            onPreChange={({index}) => this.setState({index:index})}
-            onPostChange={() => console.log('postChange')}
-            onReactive={() => console.log('postChange')}
-            position='bottom'
-            index={this.state.index}
-            renderTabs={(activeIndex, tabbar) => [
-              {
-                content: <Ons.Page key = "About"><About/></Ons.Page> ,
-                tab: <Ons.Tab label="About" icon="fa-info-circle" key = "AboutTab" />
-              },
-              {
-                content: <Ons.Page key = "Yoga"><Yoga/></Ons.Page>,
-                tab: <Ons.Tab label="Yoga" icon="fa-child" className = "testTab" key = "YogaTab"/>
-              },
-              {
-                content: <Ons.Page  key = "Breathing"><Breathing/></Ons.Page>,
-                tab: <Ons.Tab label="Breathing" icon="fa-grin" key = "BreathingTab" />
-              },
-              {
-                content: <Ons.Page  key = "Meditation"><Meditation/></Ons.Page>,
-                tab: <Ons.Tab label="Meditation" icon="fa-heart" key = "MeditationTab" />
-              }]
-            }
-          />
-        </Ons.Page>
-    );
-  }
+ 
+    componentWillMount() {
+      console.log(keys);
+        this.pubnub.subscribe({
+            channels: ['channel1'],
+            withPresence: true
+        });
+ 
+        this.pubnub.getMessage('channel1', (msg) => {
+            console.log(msg);
+        });
+ 
+        this.pubnub.getStatus((st) => {
+            this.pubnub.publish({
+                message: 'hello world from react',
+                channel: 'channel1'
+            });
+        });
+    }
+ 
+    componentWillUnmount() {
+        this.pubnub.unsubscribe({
+            channels: ['channel1']
+        });
+    }
+ 
+    render() {
+        const messages = this.pubnub.getMessage('channel1');
+        return (
+            <div>
+                <ul>
+                    {messages.map((m, index) => <li key={'message' + index}>{m.message}</li>)}
+                </ul>
+            </div>
+        );
+    }
 }
-
-export default Homescreen;
